d55b94132d4a512f6936eb9385a428aa742359c4
Aditya Kumar <asus9insane007@gmail.com>
2026-07-16 10:57:36 +0530

    Products API Assignment

--- products-api/index.js
+++ products-api/index.js
@@ -59,5 +59,5 @@ app.post("/products", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+  console.log("Server running on port 3000");
 });
